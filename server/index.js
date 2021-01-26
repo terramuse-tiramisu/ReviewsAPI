8de217851fd7f667beb7cd19862e4d5a9d335ebc
Zach Realberg <zrealberg@gmail.com>
2021-01-26 15:53:52 -0500

    ran npm install, started teh server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-
 require('dotenv').config();
 const app = require('./server.js');
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}, visit: http://localhost:${PORT}`);
