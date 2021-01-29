c73c49e15fc2e388065e1d673e08209533430128
Zach Realberg <zrealberg@gmail.com>
2021-01-28 20:13:33 -0500

    got the pipeline to work in my code, still need to format it correctly before returning to router and integrate the other inputs

--- database/models/reviewGetter.js
+++ database/models/reviewGetter.js
@@ -14,52 +14,35 @@ const reviewGetter = function(prodId, page = 1, count = 5, sort = 'helpful') {
     page: parseInt(page),
     count: parseInt(count),
     result: []
-  }
-  // console.log('packet', packet);
-  // var skipVal = packet.page * packet.count;
-  getReviewDocs(packet.product)
-    .then((data) => {
-      console.log('heres my data from getreviewDocs', data);
-      _.forEach(data, (review)=>{
-        packet.result.push({
-          "review_id": review.id,
-          "rating": review.rating,
-          "summary": review.summary,
-          "recommend": review.recommend,
-          "response": review.response,
-          "body": review.body,
-          "date": review.date,
-          "reviewer_name": review.reviewer_name,
-          "helpfulness": review.helpfulness,
-          "photos": []
-        })
-      })
-      return packet;
-    })
-    .then((packet)=> {
-      console.log('heres part of my packet', packet.result);
-      //iterate over packet.result
-      _.forEach(packet.result, (review) => {
-        //for each review, pull out review Id
-        //find all documents in the reviews_photos collection
-        return ReviewsPhotos.find({review_id: review.review_id})
-          .then((data) => {
-            console.log('this is what came back from reviewsPhotos', data);
-            // _.forEach(data, (obj))
-            // packet.results.photos.push({id: obj.id, url: obj.url})
-          })
-      })
-    //       //for each document that comes back, push {id: reviews_photos.id; url: reviews_phtos.url}
-    //   //return packet
-    // })
-    .catch((err) => {
-      console.log('err in reviewGetter.js', err)
-    })
+  };
+
+  return Reviews.aggregate([
+      {
+        $match: {
+          'product_id': packet.product
+        }
+      },
+      {
+        '$lookup': {
+          'from': 'reviews_photos',
+          'localField': 'id',
+          'foreignField': 'review_id',
+          'as': 'photos'
+        }
+      },
+      {
+        '$sample': {
+          'size': 3
+        }
+      }
+  ]).exec()
+  .then((result) => {
+    console.log('pipeline result:', result);
+  })
+  .catch((err) => {
+    console.log('err in reviewGetter.js', err)
+  })
 };
 
-const getReviewDocs = function(productId) {
-  console.log('proudct id', productId);
-  return Reviews.find({"product_id": productId}).exec()
-}
 
 module.exports = reviewGetter;
